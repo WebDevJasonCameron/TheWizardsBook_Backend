9d5cfed9dd418c8263b7d1ea3ac81166e9b7b23c
Jason Cameron <dev.jasoncameron@gmail.com>
2026-04-02 20:01:46 -0500

    starting new project for backend wiz book

---
+++ src/main/java/com/smashingwizards/thewizardsbook_backend/TheWizardsBookBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smashingwizards.thewizardsbook_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheWizardsBookBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheWizardsBookBackendApplication.class, args);
+    }
+
+}
